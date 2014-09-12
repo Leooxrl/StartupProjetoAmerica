38153ab4809d2f14666196ec746b050e8fa7aa44
Leonardo Leandro <leonardo.rox1@gmail.com>
2014-09-11 21:15:53 -0300

    alteração na classe

--- ProjetoTestGitHub/src/br/com/startupamerica/teste/github/HelloWorld.java
+++ ProjetoTestGitHub/src/br/com/startupamerica/teste/github/HelloWorld.java
@@ -2,6 +2,10 @@ package br.com.startupamerica.teste.github;
 
 public class HelloWorld {
 
+	/**
+	 * 
+	 * primeiro fonte alterado
+	 */
 	public static void main(String[] args) {
 
 		System.out.println("Ola mundo!!!");
